d21be074e8cfb4cffff813970ca14648eef17f37
せいい <superstarzsw@gmail.com>
2021-03-18 15:40:32 +0900

    feat: 实现了web端的代码

---
+++ beautiful-dnd-across-parent-demo/src/App.tsx
@@ -0,0 +1,159 @@
+import React, { FC, useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import ServiceCommandUnit from './ServiceCommandUnit';
+import { static_items } from './data1';
+
+export interface Child {
+    id: number;
+    content: string;
+}
+
+interface Father {
+    id: number;
+    content: string;
+    subItems: Child[];
+}
+
+const reorderFather = (list: Father[], startIndex: number, endIndex: number) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+    return result;
+};
+const reorderChild = (list: Child[], startIndex: number, endIndex: number) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+    return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
+    userSelect: 'none',
+    padding: grid * 2,
+    margin: `0 0 ${grid}px 0`,
+    background: isDragging ? 'lightgreen' : 'grey',
+    ...draggableStyle,
+});
+
+const getListStyle = (isDraggingOver: boolean) => ({
+    background: isDraggingOver ? 'lightblue' : 'lightgrey',
+    padding: grid,
+    width: 200,
+});
+
+const App: FC = () => {
+    const [dataItems, setDataItems] = useState<Father[]>(static_items);
+
+    const onDragEnd = (result: any) => {
+        // dropped outside the list
+        console.log(result);
+        console.log('innner drag');
+        if (!result.destination) {
+            return;
+        }
+        const sourceIndex = result.source.index;
+        const destIndex = result.destination.index;
+        if (result.type === 'droppableItem') {
+            const items: Father[] = reorderFather(dataItems, sourceIndex, destIndex);
+            setDataItems(items);
+        } else if (result.type === 'droppableSubItem') {
+            const itemSubItemMap = dataItems.reduce((acc: any, item) => {
+                acc[item.id] = item.subItems;
+                //console.log('item 67:  ' + JSON.stringify(item));
+                return acc;
+            }, {});
+
+            const sourceParentId = parseInt(result.source.droppableId);
+            const destParentId = parseInt(result.destination.droppableId);
+            console.log('sourceParentId' + sourceParentId);
+            console.log('itemSubItemMap' + JSON.stringify(itemSubItemMap));
+            const sourceSubItems = itemSubItemMap[sourceParentId];
+            const destSubItems = itemSubItemMap[destParentId];
+
+            let newItems = [...dataItems];
+
+            /** In this case subItems are reOrdered inside same Parent */
+            if (sourceParentId === destParentId) {
+                const reorderedSubItems: Child[] = reorderChild(
+                    sourceSubItems,
+                    sourceIndex,
+                    destIndex,
+                );
+                newItems = newItems.map((item) => {
+                    if (item.id === sourceParentId) {
+                        item.subItems = reorderedSubItems;
+                    }
+                    return item;
+                });
+                setDataItems(newItems);
+            } else {
+                const newSourceSubItems: Child[] = [...sourceSubItems];
+                const [draggedItem] = newSourceSubItems.splice(sourceIndex, 1);
+
+                const newDestSubItems: Child[] = [...destSubItems];
+                newDestSubItems.splice(destIndex, 0, draggedItem);
+                newItems = newItems.map((item) => {
+                    if (item.id === sourceParentId) {
+                        item.subItems = newSourceSubItems;
+                    } else if (item.id === destParentId) {
+                        item.subItems = newDestSubItems;
+                    }
+                    return item;
+                });
+                setDataItems(newItems);
+            }
+        }
+    };
+
+    // Normally you would want to split things out into separate components.
+    // But in this example everything is just done in one place for simplicity
+
+    return (
+        <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="droppable" type="droppableItem">
+                {(provided, snapshot) => (
+                    <div ref={provided.innerRef} style={getListStyle(snapshot.isDraggingOver)}>
+                        {dataItems.map((item, index) => (
+                            <Draggable key={item.id} draggableId={item.id.toString()} index={index}>
+                                {(provided: any, snapshot) => (
+                                    <div>
+                                        <div
+                                            ref={provided.innerRef}
+                                            {...provided.draggableProps}
+                                            style={getItemStyle(
+                                                snapshot.isDragging,
+                                                provided.draggableProps.style,
+                                            )}
+                                        >
+                                            {item.content}
+                                            <span
+                                                {...provided.dragHandleProps}
+                                                style={{
+                                                    display: 'inline-block',
+                                                    margin: '0 10px',
+                                                    border: '1px solid #000',
+                                                }}
+                                            >
+                                                Drag
+                                            </span>
+                                            <ServiceCommandUnit
+                                                subItems={item.subItems}
+                                                type={item.id}
+                                            />
+                                        </div>
+                                        {provided.placeholder}
+                                    </div>
+                                )}
+                            </Draggable>
+                        ))}
+                        {provided.placeholder}
+                    </div>
+                )}
+            </Droppable>
+        </DragDropContext>
+    );
+};
+
+export default App;
---
+++ beautiful-dnd-across-parent-demo/src/ServiceCommandUnit.tsx
@@ -0,0 +1,77 @@
+import { FC } from 'react';
+import { Droppable, Draggable } from 'react-beautiful-dnd';
+import { Child } from './App';
+
+const grid = 8;
+
+const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
+    // some basic styles to make the items look a bit nicer
+    userSelect: 'none',
+    padding: grid * 2,
+    margin: `0 10px 10px 0`,
+    display: 'inline-flex',
+    width: '120px',
+
+    // change background colour if dragging
+    background: isDragging ? 'lightgreen' : 'grey',
+    border: '1px solid grey',
+    // styles we need to apply on draggables
+    ...draggableStyle,
+});
+
+const getListStyle = (isDraggingOver: boolean) => ({
+    background: isDraggingOver ? 'lightblue' : 'lightgrey',
+    padding: grid,
+    margin: '10px 0',
+});
+
+interface ChildProps {
+    subItems: Child[];
+    type: number;
+}
+
+const ServiceCommandUnit: FC<ChildProps> = (props) => {
+    // Normally you would want to split things out into separate components.
+    // But in this example everything is just done in one place for simplicity
+
+    return (
+        <Droppable droppableId={props.type.toString()} type={`droppableSubItem`}>
+            {(provided, snapshot) => (
+                <div ref={provided.innerRef} style={getListStyle(snapshot.isDraggingOver)}>
+                    {props.subItems.map((item, index) => (
+                        <Draggable key={item.id} draggableId={item.id.toString()} index={index}>
+                            {(provided: any, snapshot) => (
+                                <div style={{ display: 'flex' }}>
+                                    <div
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        style={getItemStyle(
+                                            snapshot.isDragging,
+                                            provided.draggableProps.style,
+                                        )}
+                                    >
+                                        {item.content}
+                                        <span
+                                            {...provided.dragHandleProps}
+                                            style={{
+                                                display: 'block',
+                                                margin: '0 10px',
+                                                border: '1px solid #000',
+                                            }}
+                                        >
+                                            Drag
+                                        </span>
+                                    </div>
+                                    {provided.placeholder}
+                                </div>
+                            )}
+                        </Draggable>
+                    ))}
+                    {provided.placeholder}
+                </div>
+            )}
+        </Droppable>
+    );
+};
+
+export default ServiceCommandUnit;
